1820e47ee47e43cedccc3f76f57009d71eea6650
Sarah Howe <sarah@keithmcmillen.com>
2013-09-16 22:50:37 +0000

    created the project for the Advanced Editor.  Added a ui file for settings, modlines, and the key windows.
    git-svn-id: svn://chopstick.local/softstep_editor@30 b087e19c-e0c3-4903-a6fc-ecf072d2c2be

---
+++ standalone/advanced/mainwindow.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2025 KMI Music, Inc.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
